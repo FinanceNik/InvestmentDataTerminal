9c37325d762135de50cd488d2d215ec82baa783d
FinanceNik <niklas.clasen99@gmail.com>
2022-06-28 18:51:07 +0200

    functioning script plus color coded table and new dir

--- idt.py
+++ idt.py
@@ -1,14 +1,19 @@
-#!/usr/bin/env python
-
-import pandas as pd
-import numpy as np
+import random
+import time
+from datetime import date
 import pandas_datareader.data as web 
 from datetime import date, timedelta
-from time import sleep
-from tabulate import tabulate
+from rich.live import Live
+from rich.table import Table
 
 
-def main():
+def generate_table() -> Table:
+    table = Table()
+    table.add_column("Name")
+    table.add_column("Adj Close")
+    table.add_column("Daily Change")
+    table.add_column("Volume")
+
     today = date.today()
     file1 = open('config.txt', 'r')
     lines = file1.readlines()
@@ -17,26 +22,40 @@ def main():
     for line in lines[2:]:
         assets.append(line.strip())
 
+    colors = []
     tableData = []
-    # tableData.append(['Name', 'Adj. Close', 'Volume'])
-    # tableData.append(['-------------------', '-------------------', '-------------------'])
     for asset in assets:
-        rowData = []
-        data = web.get_data_yahoo(asset, start=today - timedelta(1), end=today)[-1:]
-        assetName = str(asset)
-        adjClose = round(float(data['Adj Close']), 2)
-        volume = int(data['Volume'])
-        rowData.append(assetName)
-        rowData.append(adjClose)
-        rowData.append(volume)
-        tableData.append(rowData)
+        try:
+            rowData = []
+            data = web.get_data_yahoo(asset, start=today - timedelta(1), end=today)
+            dataCurr = data[-1:]
+            dataPrev = data[-2:-1]
+            assetName = str(asset)
+            adjClose = round(float(dataCurr['Adj Close']), 4)
+            dailyChange = round((int(dataCurr['Adj Close']) - int(dataPrev['Adj Close'])) / int(dataPrev['Adj Close']) * 100, 4)
+            dailyChangeStr = f"{round((int(dataCurr['Adj Close']) - int(dataPrev['Adj Close'])) / int(dataPrev['Adj Close']) * 100, 4)}%"
+            volume = int(dataCurr['Volume'])
+            rowData.append(assetName)
+            rowData.append(adjClose)
+            rowData.append(dailyChangeStr)
+            rowData.append(volume)
+            tableData.append(rowData)
+            if dailyChange > 0.0:
+                colors.append('green')
+            else:
+                colors.append('red')
+        except:
+            pass
 
-    # for row in tableData:
-    # print("{: >20} {: >20} {: >20}".format(*row), end="\r")
 
-    print(tabulate(tableData, headers=['Name', 'Adj. Close', 'Volume']), end='\r')
+    for i, row in enumerate(tableData):
+        table.add_row(
+            f"{tableData[i][0]}", f"{tableData[i][1]}", f"[{colors[i]}]{tableData[i][2]}", f"{tableData[i][3]}"
+        )
+    return table
 
 
-if __name__=='__main__':
-    while True:
-        main()
+with Live(generate_table(), refresh_per_second=4) as live:
+    for _ in range(99999999999999999):
+        time.sleep(0.4)
+        live.update(generate_table())
